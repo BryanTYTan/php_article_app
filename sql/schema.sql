310284735b6a1233c980c677c04e32448e7fc606
BryanTYTan <90520318+BryanTYTan@users.noreply.github.com>
2025-11-09 22:00:43 +0800

    Fix syntax error on schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,33 +2,32 @@ CREATE SCHEMA article_app;
 SET search_path TO article_app;
 
 -- User Table
-CREATE TABLE users {
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    is_administrator BOOLEAN DEFAULT FALSE,
-};
+    is_administrator BOOLEAN DEFAULT FALSE
+);
 
 -- Article Table
-CREATE TABLE articles {
+CREATE TABLE articles (
     article_id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT NOW(),
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
-    creator_id INT NOT NULL REFERENCES users(user_id),
-};
+    creator_id INT NOT NULL REFERENCES users(user_id)
+);
 
 -- Comment Table
-CREATE TABLE comments {
+CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT NOW(),
     content TEXT NOT NULL,
     creator_id INT NOT NULL REFERENCES users(user_id),
-    article_id INT NOT NULL REFERENCES articles(article_id),
-}
-
+    article_id INT NOT NULL REFERENCES articles(article_id)
+);
 
 -- Indexes to speed up lookups / filters
 CREATE INDEX idx_article_title on articles(title);
